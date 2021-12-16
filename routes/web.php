5dd2002abdc751786282df766557ce12ea90e9cb
lumilio <logistic-division@lumilium.com>
2021-12-16 23:22:44 +0100

    print hello world

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'hello world';
 });
+
+/* Route::get('/home', function () {
+    return view('home');
+}); */
+
+
